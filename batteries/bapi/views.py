faa783abf0f2025be52ddf24bb18d82636bc6406
Rob McQueen <robmcqn@gmail.com>
2012-06-24 00:14:27 -0700

    adding standard url for accessing multiple providers

--- batteries/bapi/views.py
+++ batteries/bapi/views.py
@@ -3,13 +3,11 @@ from django.template import RequestContext
 from django.http import HttpResponse, Http404
 from django.conf import settings
 
-from batteries.bapi.utils import provider_url_generators
+from batteries.bapi.utils import provider_request_map
 
 import grequests
-import requests
+
 import urllib
-import eventful
-import eventbrite
 import time
 import json
 
@@ -21,19 +19,29 @@ def events(request):
     lat = request.GET.get("lat")
     lon = request.GET.get("lon")
     cur_time = int(time.time()*1000)
+    provider = request.GET.get("provider")
+
+    if not provider in provider_request_map.keys():
+        raise Http404("Provider not available")
+
+    request_func = provider_request_map[provider]
+    data = request_func(lat,lon)
+    return HttpResponse(json.dumps({'results':data}))
+    
+    
     
     #urls = [url_gen(lat,lon,cur_time) for url_gen in provider_url_generators.values()]
         
-    payload = {'query':'select eid,start_time,end_time,location,name,description,pic_square from event where eid=209798352393506','format':'json'}
-    url = "https://api.facebook.com/method/fql.query?%s" % (urllib.urlencode(payload))
+    # payload = {'query':'select eid,start_time,end_time,location,name,description,pic_square from event where eid=209798352393506','format':'json'}
+    # url = "https://api.facebook.com/method/fql.query?%s" % (urllib.urlencode(payload))
 
-    urls = [url]
+    # urls = [url]
 
-    rs = (grequests.get(u) for u in urls)
-    results = grequests.map(rs)
-    result = json.loads(results[0].text)
-    result_json = {'results':[result[0],result[0],result[0],result[0]]}
-    return HttpResponse(json.dumps(result_json))
+    # rs = (grequests.get(u) for u in urls)
+    # results = grequests.map(rs)
+    # result = json.loads(results[0].text)
+    # result_json = {'results':[result[0],result[0],result[0],result[0]]}
+    # return HttpResponse(json.dumps(result_json))
 
 def events_eventful(request):
     lat = request.GET.get("lat")
