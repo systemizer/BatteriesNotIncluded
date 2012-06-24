85f59799c1f2d8dc8ad7c6d679f1aaf1921d98a0
Jeff Hui <jeff@jeffhui.net>
2012-06-23 19:52:24 -0700

    Added static paths to local_settings_template.py

--- batteries/local_settings_template.py
+++ batteries/local_settings_template.py
@@ -1,3 +1,6 @@
+import os
+rel = lambda *x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -11,13 +14,13 @@ DATABASES = {
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/path/to/media/'
+MEDIA_ROOT = rel('static', 'uploads')
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/path/to/static/'
+STATIC_ROOT = rel('static', 'root')
 
 FACEBOOK_APP_ID = ''
 FACEBOOK_APP_SECRET = ''
