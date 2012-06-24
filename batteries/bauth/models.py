9c9b2371502d09aa6ff54e58a9b1e0ef2fbed1a8
Rob McQueen <robmcqn@gmail.com>
2012-06-24 02:28:25 -0700

    integrating facebook

--- batteries/bauth/models.py
+++ batteries/bauth/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django_facebook.models import FacebookProfileModel
-
-class BatteriesProfile(FacebookProfileModel):
-    user = models.OneToOneField('auth.User')
-
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+
+
+class BatteriesProfile(FacebookProfileModel):
+    user = models.OneToOneField(User)
+
 
 #Make sure we create a MyCustomProfile when creating a User
 def create_facebook_profile(sender, instance, created, **kwargs):
