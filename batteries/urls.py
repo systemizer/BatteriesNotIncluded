4c1d6991f69a0637164fedc293138d8daf95fef2
Jeff Hui <jeff@jeffhui.net>
2012-06-23 19:53:26 -0700

    Made base url the core template.

--- batteries/urls.py
+++ batteries/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns(
-    '',
     url(r'^api/', include('batteries.bapi.urls')),
     url(r'^facebook/', include('django_facebook.urls')),
     url(r'^accounts/', include('django_facebook.auth_urls')),
     url(r'^admin/', include(admin.site.urls)),
-    )
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+)
